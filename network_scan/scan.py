f029ed415c13b9de0a2ed5a2899dccc46c6a1cc6
russeii <rratcliffe57@gmail.com>
2016-08-17 09:45:15 +0000

    added second mac

--- network_scan/scan.py
+++ network_scan/scan.py
@@ -8,12 +8,15 @@ class findRoomates():
     def find_roomates(self):
         Roos="Russell is not home"
         Grace="Grace is not in my appartment"
-        subprocess.call("echo milk | sudo -S arp-scan --localnet  > allout.txt 2>&1", shell=True)
+        subprocess.call("echo raspberry | sudo -S arp-scan --localnet  > allout.txt 2>&1", shell=True)
         if '64:bc:0c:65:43:22' in open('allout.txt').read():
             Roos="Russell is home"
 
+	if '00:a0:c6:eb:5c:6f' in open('allout.txt').read():
+	    Roos="Russell is home"
 
-            if '12:b3:b7:d2:e2:3d' in open('allout.txt').read():
+
+        if '12:b3:b7:d2:e2:3d' in open('allout.txt').read():
                 Grace="Grace is at my appartment"
         AJ="AJ is not Home"
         Orion="Orion is not home"
