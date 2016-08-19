0913e185f3fa9a5b10d20d4c4d85e899ce967d3b
russeii <rratcliffe57@gmail.com>
2016-08-18 20:46:28 -0400

    made it mobile friendly

--- network_scan/scan.py
+++ network_scan/scan.py
@@ -8,18 +8,26 @@ class findRoomates():
     def find_roomates(self):
         Roos="Russell is not home"
         Grace="Grace is not in my appartment"
+        AJ="AJ is not Home"
+        Orion="Orion is not home"
         subprocess.call("echo raspberry | sudo -S arp-scan --localnet  > allout.txt 2>&1", shell=True)
+        
         if '64:bc:0c:65:43:22' in open('allout.txt').read():
             Roos="Russell is home"
 
-	if '00:a0:c6:eb:5c:6f' in open('allout.txt').read():
-	    Roos="Russell is home"
+        if '00:a0:c6:eb:5c:6f' in open('allout.txt').read():
+            Roos="Russell is home"
+
+        if '00:a0:c6:eb:5c:6ftest' in open('allout.txt').read():
+            AJ="AJ is home"
+
+        if '00:a0:c6:eb:5c:6ftest' in open('allout.txt').read():
+            Orion="Orion is home"
 
 
         if '12:b3:b7:d2:e2:3d' in open('allout.txt').read():
-                Grace="Grace is at my appartment"
-        AJ="AJ is not Home"
-        Orion="Orion is not home"
+        Grace="Grace is at my appartment"
+        
 
         roomates = [
         {"room":Roos,},
